fd8375a7179e28a7ec8b9b4565fbc9f9096e18e0
tokslaw <tokslaw@chiefsoft.com>
2021-11-13 20:58:25 -0500

    Fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed  from './components/ChatFeed';
+
 import './App.css';
 
 function App() {
@@ -9,7 +11,8 @@ function App() {
       height="100vh"
       projectID="e6686382-6547-4837-9206-c5bdca8291dc"
       userName="tokslaw"
-      userSecret="Tokslaw001"   
+      userSecret="Tokslaw001"  
+      renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps }/> } 
 
     />
       
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,11 @@
+const ChatFeed =(props) => {
+    console.log (props);
+
+    return (
+        <div>
+            Taborg ChatFeed
+        </div>
+    )
+}
+
+export default ChatFeed;
