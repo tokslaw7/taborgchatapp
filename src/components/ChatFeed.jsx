cb8c419fcdd5bf4204e27ba651e49f1cbe07f008
tokslaw <tokslaw@chiefsoft.com>
2021-11-14 14:21:32 -0500

    chatfeed updated

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -8,7 +8,23 @@ import TheirMessage from "./TheirMessage";
 const ChatFeed = (props) => {
     const { chats, activeChat, userName, messages } = props;
 
-    const chat = chats && chats [activeChat];
+    const chat = chats && chats[activeChat];
+
+    const renderReadReceipts = (message, isMyMessage) => {
+       return chat.people.map((person, index) => person.last_read === message.id && (
+            <div
+                key={`read_${index}`} 
+                className="read-receipt"
+                style={{
+                    float: isMyMessage ? 'right' : left,
+                    backgroundImage: `url(${person?.person?.avatar})`
+                }}
+            />
+        ))
+
+        
+
+    }
 
     const renderMessages = () => {
         const keys = Object.keys(messages);
@@ -29,7 +45,7 @@ const ChatFeed = (props) => {
                 }
                 </div>
                 <div className="read-receipts" style ={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
-                    read receipts
+                    {renderReadReceipts(message, isMyMessage)}
                 </div>
                 </div>
             )
