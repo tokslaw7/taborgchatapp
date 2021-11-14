5a5caa41b1f5c71d70f0618254d2ffaaeeff91f7
tokslaw <tokslaw@chiefsoft.com>
2021-11-13 20:39:28 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+function App() {
+  return (
+    <ChatEngine 
+    
+      height="100vh"
+      projectID="e6686382-6547-4837-9206-c5bdca8291dc"
+      userName="tokslaw"
+      userSecret="Tokslaw001"   
+
+    />
+      
+  );
+}
+
+export default App;
